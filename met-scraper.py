4440365c88cbe7d7e450837a51a98fe4f55fa194
gabrielibagon <gabriel.ibagon@gmail.com>
2017-12-30 09:06:18 -0800

    committing the uncomitted

--- met-scraper.py
+++ met-scraper.py
@@ -1,28 +1,34 @@
 import csv
 import json
 import urllib.request
-import os.path
+import os
 
-baseURL = "http://www.metmuseum.org/api/Collection/additionalImages?crdId="
+base_url = "http://www.metmuseum.org/api/Collection/additionalImages?crdId="
+artwork_dir = 'artworks/'
 
 with open("MetObjects.csv",'r') as f:
 	reader = csv.reader(f,delimiter=',')
+	lastImg = sorted([int(x.split('.')[0]) for x in os.listdir('artworks')])[-1]
 	next(reader)
 	for line in reader:
-		publicDomain = line[2]
-		if publicDomain:
-			itemID = line[3]
-			fullURL = baseURL + itemID
-			jsonResponse = json.loads(urllib.request.urlopen(fullURL).read().decode('utf-8'))['results']
-			if jsonResponse:
-				imageURL = jsonResponse[0]['webImageUrl']
-				title = jsonResponse[0]['title']
-				if not title:
-					title = itemID
-				filename = 'artworks/' + title + '.jpg'
-				if not os.path.isfile(filename):
-
-					try:
-						urllib.request.urlretrieve(imageURL,filename)
-					except Exception as e:
-						print(e)
+		itemID = line[3].zfill(7)
+		if int(itemID) <= int(lastImg):
+			continue
+		print(line)
+		fullURL = base_url + itemID
+		jsonResponse = json.loads(urllib.request.urlopen(fullURL).read().decode('utf-8'))['results']
+		if jsonResponse:
+			imageURL = jsonResponse[0]['webImageUrl']
+			filename = itemID + '.jpg'
+			filepath = os.path.join(artwork_dir,filename)
+			if not os.path.isfile(filepath):
+				try:
+					urllib.request.urlretrieve(imageURL,filepath)
+					print('Successfully Downloaded')
+				except Exception as e:
+					print(e)
+			else:
+				print("File already exists")
+		else:
+			print('Invalid file')
+		print('\n')
